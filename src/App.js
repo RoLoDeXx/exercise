1dd76d98d2b591b4536ececb0dcc3dfdd30db9ca
Samarth Sharma <dexxrolo@gmail.com>
2022-09-15 12:18:06 +0530

    Feat: assets and other details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import ExerciseDetals from "./components/ExerciseDetails";
+import Landing from "./components/Landing";
+import Navbar from "./components/common/Navbar";
+import Footer from "./components/common/Footer";
 
 const App = () => {
-  return <div>HELLO</div>;
+  return (
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/exercise/:id" element={<ExerciseDetals />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
