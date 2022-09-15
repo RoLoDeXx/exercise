c9ce270f609d6751a1b9263888913e2ee0baddd3
Samarth Sharma <dexxrolo@gmail.com>
2022-09-15 13:28:07 +0530

    Feat: banner, search and workouts components

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import Banner from "./Banner";
+import SearchExercise from "./SearchExercise";
+import Workouts from "./Workouts";
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <Box>
+      <Banner />
+      <SearchExercise />
+      <Workouts />
+    </Box>
+  );
 };
 
 export default index;
--- src/components/common/Navbar/index.js
+++ src/components/common/Navbar/index.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+import Logo from "../../../assets/images/Logo.png";
+import styles from "./index.module.css";
 
 const index = () => {
-  return <div>Navbar</div>;
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-around"
+      sx={{
+        gap: { sm: "122px", xs: "40px" },
+        mt: {
+          sm: "32px",
+          xs: "20px",
+        },
+        justifyContent: "none",
+        px: "20px",
+      }}
+    >
+      <Link to="/">
+        <img className={styles.logo} src={Logo} alt="logo" />
+      </Link>
+      <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
+        <Link to="/" className={styles.anchor}>
+          Home
+        </Link>
+        <a className={styles.anchor} href="#exercises">
+          Workouts
+        </a>
+        <Link to="/"></Link>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default index;
